a4984eaa409254b448233d05a48500705b1e1fea
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-06 19:31:12 +0100

    Methods and functions added to scroll pictures & style CSS

--- action.js
+++ action.js
@@ -4,11 +4,25 @@ BONUS: Aggiungere un’immagine presa anch’essa da un data
 */
 
 var app = new Vue({
+
   el: '#root',
   data: {
     main_title: 'vue js',
     subtitle: 'JavaScript Framework',
     url_meme_got: 'img/meme_got.png',
     url_meme_react: 'img/meme_react.jpg',
-  }
+    display_img_got: 'show',
+    display_img_react: 'hidden',
+  },
+  methods: {
+    hideImg: function() {
+      if (this.display_img_got === 'show') {
+        this.display_img_got = 'hidden';
+        this.display_img_react = 'show';
+      } else {
+        this.display_img_got = 'show';
+        this.display_img_react = 'hidden';
+      }
+    },
+  },
 });
